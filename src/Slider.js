e0c50cbe8f45e9a8ac9d3e3a9447ccd15dafd817
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2024-08-24 14:35:23 +0530

    Slider.js

--- src/Slider.js
+++ src/Slider.js
@@ -51,7 +51,7 @@ export default function Sliding() {
   return (
     <>
       {/* Slider */}
-      <div className="relative pt-[85px]">
+      <div className="relative ">
         <Slider {...sliderSettings}>
           {images.map((src, index) => (
             <div key={index} className="relative">
