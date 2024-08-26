7b912998556770b465c855df692b13699ced79e1
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2024-08-26 11:25:04 +0530

    HealthCheck.js

--- src/HealthCheck.js
+++ src/HealthCheck.js
@@ -25,7 +25,7 @@ function HealthCheck() {
           <div>
             <div className="relative">
               <img   loading='lazy' className="w-full hidden md:block" src="https://cdn.shopify.com/s/files/1/0674/9614/9171/files/csr_banner_2_1.jpg?v=1724326525" alt="Slide 1" />
-              <img   loading='lazy' className="w-full block md:hidden" src="https://cdn.shopify.com/s/files/1/0674/9614/9171/files/csr_banner_2_mobile.jpg?v=1724313279" alt="Slide 1" />
+              <img   loading='lazy' className="w-full block md:hidden" src="https://cdn.shopify.com/s/files/1/0674/9614/9171/files/csr_banner_2_mobile_2.jpg?v=1724651650" alt="Slide 1" />
              
             </div>
            
