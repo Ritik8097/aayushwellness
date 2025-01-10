af8dc9fd82c2c878681b597a855252458bb13f0a
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-01-10 12:54:27 +0530

    Slider.js

--- src/Slider.js
+++ src/Slider.js
@@ -115,28 +115,36 @@ export default function AnimatedSlider() {
 
   const mobileImages = [
     {
-      src: 'https://cdn.shopify.com/s/files/1/0674/9614/9171/files/BANNER_2-1.jpg?v=1732362895',
+      src: 'https://cdn.shopify.com/s/files/1/0653/9830/9053/files/BANNER_5_new.jpg?v=1736493684',
       heading: 'Life Long Wellness',
       subtext: 'Taste Wellness , Embrace Nutrition : Aayush Your Gatway To Good Health ',
       url:"/wellness/health-wellness",
       buttonText: 'Explore more',
     },
+   
     {
-      src: 'https://cdn.shopify.com/s/files/1/0674/9614/9171/files/BANNER_3-1.jpg?v=1732362895',
-      heading: 'Your Health , Our Priority',
+      src: 'https://cdn.shopify.com/s/files/1/0653/9830/9053/files/BANNER_5-2_new.jpg?v=1736493685',
+     heading: 'Your Health , Our Priority',
       subtext: 'Tobacco-Free, Ayurveda Rich Solution',
       url:"/wellness/modern-science",
       buttonText: 'View details',
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0674/9614/9171/files/BANNER_4-1.jpg?v=1732362895',
-      heading: 'Introducing Gummies',
-      subtext: 'That Go Beyond Ordinary',
-      url:"/gummies",
-      buttonText: 'View details',
+      src: 'https://cdn.shopify.com/s/files/1/0653/9830/9053/files/Beauty_Gummies_banner_-_Product_Page_-_18-10-2024_-_mobile_size_1_7d193c20-ac44-40a8-b2bb-426116e22f07.gif?v=1736493687',
+      heading: '',
+      subtext: '',
+      url:"https://aayushlife.com/",
+      buttonText: 'Buy Now',
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0674/9614/9171/files/BANNER_1-1.jpg?v=1732362895',
+      src: 'https://cdn.shopify.com/s/files/1/0653/9830/9053/files/Beauty_Gummies_banner_-_Product_Page_-_18-10-2024_-_mobile_size_1.gif?v=1736493687',
+      heading: '',
+      subtext: '',
+      url:"https://aayushlife.com/",
+      buttonText: 'Buy Now',
+    },
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0653/9830/9053/files/BANNER_5-5_new.jpg?v=1736493684',
       heading: 'Transforming Habits ',
       subtext: 'Transforming Lives',
       url:"/ayurveda",
