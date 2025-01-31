a543cfcb3184caa48c45bdedfafeeec8b7e70c5e
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-01-31 17:32:36 +0530

    FooterCopyright.js

--- src/FooterCopyright.js
+++ src/FooterCopyright.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FooterCopyright() {
   return (
     <div className=' text-center pt-3 pb-3 ' style={{borderTop:"1px solid rgba(210, 210, 210, .5)",background:'#fcfaf8',color:'#707070'}}>
-      Copyright © 2024 Aayush.
+      Copyright © 2025 Aayush.
     </div>
   )
 }
