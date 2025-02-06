ccbc56b3d98ee7c944e2817d3117e29c8aa2ce0a
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-06 14:49:39 +0530

    Create Health.js

---
+++ src/Health.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Banner from './Banner'
+import Specialities from './Specilalities'
+
+import Service from './service'
+
+
+const items = [
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text: ["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+  { text:["Aayush Wellness Healthcare Offers World-Class Healthcare Services in Mumbai Suburbs with Affordable Medical Treatments", ] },
+];
+
+const Health = () => {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+   
+    <Specialities/>
+    <Service/>
+     
+
+    <Footer/>
+    </>
+  )
+}
+
+export default Health
