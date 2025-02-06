9563f529347df95526fd1194231a218a4df6d2c3
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-06 18:53:16 +0530

    Update VentureSection1.js

--- src/VentureSection1.js
+++ src/VentureSection1.js
@@ -13,14 +13,15 @@ const VentureSection1 = () => {
 
   return (
     <div  className={`containe ${animate ? "animate" : ""}` }>
-      <h1 class="text" >Digital. Brand. Accelerator.</h1>
+      <h1 class="text" >Innovate. Scale. Transform.</h1>
      
       <div className="overlay"></div>
        {/* Get in Touch Button */}
-       <button className="absolute bottom-10 left-10 md:left-24 flex items-center gap-2 px-6 py-3 text-black bg-white rounded-full text-lg font-medium hover:scale-105 transition">
+       <a href="https://wa.me/918655900409?text=https://www.aayushwellness.com/aayush-venture Hi">
+       <button  className="absolute bottom-10 left-10 md:left-24 flex items-center gap-2 px-6 py-3 text-black bg-white rounded-full text-lg font-medium hover:scale-105 transition">
   <span>➝</span> Get in touch
 </button>
-
+</a>
       {/* Scroll Down Button 
       <button className="absolute bottom-10 right-10 w-14 h-14 flex items-center justify-center rounded-full border border-white text-white text-2xl hover:scale-105 transition">
         ⬇
