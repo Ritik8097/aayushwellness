664a7f7ed5fecf88149875a3bb77884ec7aa5f1f
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-07 17:54:39 +0530

    Update Banner2.js

--- src/Banner2.js
+++ src/Banner2.js
@@ -5,31 +5,58 @@ import './Banner2.css'
 const Banner2 = () => {
   return (
     <div className="first-facility-area snipcss-kcLyL">
-  <div className="row">
-    <div className="col-lg-4 col-sm-6">
-      <div className="first-facility-item"><i className="flaticon-support" />
-        <h3>Instant Doctor Consultation</h3>
-        <p>Consult Best Doctors of India Online in 5 minutes.</p>
+      <div className="row">
+        <div className="col-lg-4 col-sm-6">
+          <div className="first-facility-item">
+            <div className="facility-content">
+              <img 
+                src="https://cdn.shopify.com/s/files/1/0653/9830/9053/files/icons_D1.png?v=1738927081" 
+                alt="Instant Doctor Consultation Icon" 
+                className="facility-icon"
+              />
+              <h3>Instant Doctor Consultation</h3>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-sm-6 offset-sm-3 offset-lg-0">
+          <div className="first-facility-item">
+            <div className="facility-content">
+              <img 
+                src="https://cdn.shopify.com/s/files/1/0653/9830/9053/files/icons_D2.png?v=1738927080" 
+                alt="Doctors Serve Online Icon" 
+                className="facility-icon"
+              />
+              <h3>Doctors, Serve Online</h3>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-sm-6">
+          <div className="first-facility-item">
+            <div className="facility-content">
+              <img 
+                src="https://cdn.shopify.com/s/files/1/0653/9830/9053/files/icons_D3.png?v=1738927080" 
+                alt="Digital Clinics of India Icon" 
+                className="facility-icon"
+              />
+              <h3>Digital Clinics of India</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="shape">
+        <div id="style-sX8cX" className="style-sX8cX">
+          <img 
+            alt="Shape Image" 
+            src="https://www.xpresscure.com/_next/image?url=%2Fimg%2Fshape%2Fshape1.png&w=3840&q=75" 
+            decoding="async" 
+            data-nimg="fill" 
+            id="style-CEyeB" 
+            className="style-CEyeB" 
+          />
+        </div>
       </div>
     </div>
-    <div className="col-lg-4 col-sm-6 offset-sm-3 offset-lg-0">
-      <div className="first-facility-item"><i className="flaticon-online-learning" />
-        <h3>Doctors, Serve Online</h3>
-        <p>Doctors, reach out to patients across India, at zero investment.</p>
-      </div>
-    </div>
-    <div className="col-lg-4 col-sm-6">
-      <div className="first-facility-item"><i className="flaticon-care" />
-        <h3>Digital Clinics of India</h3>
-        <p>Health workers, become a Doctor Mitra today &amp; enhance your dignity.</p>
-      </div>
-    </div>
-  </div>
-  <div className="shape">
-    <div id="style-sX8cX" className="style-sX8cX"><img alt="Image" src="https://www.xpresscure.com/_next/image?url=%2Fimg%2Fshape%2Fshape1.png&w=3840&q=75" decoding="async" data-nimg="fill" id="style-CEyeB" className="style-CEyeB" /><noscript /></div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
 export default Banner2;
