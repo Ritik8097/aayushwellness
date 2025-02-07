ec5632902c93c2ba6039f5e776d796dd47ab40d0
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-07 17:55:52 +0530

    Update Service.js

--- src/Service.js
+++ src/Service.js
@@ -9,105 +9,94 @@ const Service = () => {
     <h2>Our Services</h2>
    
   </div>
-    <div className="d-flex flex-row justify-content-center flex-wrap snipcss-Pk3Di">
-    <div className="work-bx">
+    <div  
+    style={{
+          backgroundImage: "url('https://cdn.shopify.com/s/files/1/0653/9830/9053/files/2.jpg?v=1738915712')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+        
+          scrollSnapType:"x mandatory"
+        }} className="d-flex  overflow-scroll   css-Pk3Di  justify-start md:justify-center md:flex-wrap relative z-[5] md:min-h-[100vh] min-h-full">
+   
+    <div className="work-bx"> 
       <div className="work-num-bx">01</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">General Medicine and Critical Care</h5>
-          <p>Advanced ICU</p>
+          <h5 className="title text-dark mb-10">Children's Healthcare</h5>
+          <p>Comprehensive care for pediatric patients, ensuring the health and happiness of your little ones.</p>
         </div>
-        <a href="/verticals/intensive-care-and-general-medicine/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/intensive-care-and-general-medicine/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">02</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">Children's HealthCare</h5>
-          <p>All Care - Children related</p>
+          <h5 className="title text-dark mb-10">Women's Healthcare</h5>
+          <p>Specialized services addressing the unique health needs of women at every stage of life.</p>
         </div>
-        <a href="/verticals/child-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/child-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">03</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">Women's HealthCare</h5>
-          <p>All Care - for Women</p>
+          <h5 className="title text-dark mb-10">Laparoscopic<br />Surgery</h5>
+          <p>Minimally invasive procedures for abdominal and thoracic conditions, promoting quicker recovery times.</p>
         </div>
-        <a href="/verticals/women-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/laparoscopic-surgery/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">04</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">Laparoscopic<br />Surgery</h5>
-          <p>Abdominal and Thoracic Regions</p>
+          <h5 className="title text-dark mb-10">Urology <br />Procedure</h5>
+          <p> Expert care for kidney, bladder, and prostate issues, utilizing the latest medical advancements.</p>
         </div>
-        <a href="/verticals/laparoscopic-surgery/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/urology/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">05</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">Urology <br />Procedure</h5>
-          <p>Everything Kidneys to Prostate</p>
+          <h5 className="title text-dark mb-10">Orthopaedic <br />Surgery</h5>
+          <p> Treatment for bones, ligaments, and muscles, restoring mobility and quality of life.</p>
         </div>
-        <a href="/verticals/urology/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/orthopaedics/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">06</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">Orthopaedic <br />Surgery</h5>
-          <p>Bones, Ligaments and Muscles</p>
+          <h5 className="title text-dark mb-10">ENT &amp; Airway<br />Surgery</h5>
+          <p>Advanced endoscopic and microsurgical techniques for ear, nose, and throat conditions.</p>
         </div>
-        <a href="/verticals/orthopaedics/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/ent-surgery/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">07</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">Cosmetic<br />Surgery</h5>
-          <p>Aesthetic and Reconstructive Plastic Surgery.</p>
+          <h5 className="title text-dark mb-10">Cancer Care</h5>
+          <p>Comprehensive oncology services, providing support and treatment for various types of cancer.</p>
         </div>
-        <a href="/verticals/cosmetic-surgery/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/cancer-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
     <div className="work-bx">
       <div className="work-num-bx">08</div>
       <div className="for">
         <div className="work-content">
-          <h5 className="title text-dark mb-10">ENT &amp; Airway<br />Surgery</h5>
-          <p>Endoscopy and Microsurgery</p>
-        </div>
-        <a href="/verticals/ent-surgery/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
-      </div>
-    </div>
-    <div className="work-bx">
-      <div className="work-num-bx">09</div>
-      <div className="for">
-        <div className="work-content">
-          <h5 className="title text-dark mb-10">Cancer Care</h5>
-          <p>A-Z Cancer Care for All.</p>
-        </div>
-        <a href="/verticals/cancer-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
-      </div>
-    </div>
-    <div className="work-bx">
-      <div className="work-num-bx">10</div>
-      <div className="for">
-        <div className="work-content">
           <h5 className="title text-dark mb-10">Day Care<br />Surgeries</h5>
-          <p>Providing Day-Care</p>
+          <p>Efficient and safe day-care surgical procedures for a range of conditions.</p>
         </div>
-        <a href="/verticals/day-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a>
+        {/* <a href="/verticals/day-care/" className="btn btn-success">View <i className="btn-icon-bx fas fa-chevron-right" /></a> */}
       </div>
     </div>
   </div>
