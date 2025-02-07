d6e5549c13f60cc9cf0cbc9084fb79b1bf52a5d5
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-07 17:53:50 +0530

    Update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -3,9 +3,9 @@ import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Banner2 from './Banner2';
 
-// Slider settings
+
+
 const sliderSettings = {
 //   dots: true,
   infinite: true,
@@ -13,7 +13,7 @@ const sliderSettings = {
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 7000,
   arrows: false,
   pauseOnHover: false,
 };
@@ -22,22 +22,24 @@ const Banner = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const videoRefs = useRef([]);
 
-  // Update state on window resize
+  
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  // Mobile and desktop content (images & videos)
+  
   const mobileContent = [
-    { type: 'image', src: "https://cdn.shopify.com/s/files/1/0653/9830/9053/files/Slider_banner_2.jpg?v=1738756117" },
-    
+    { type: 'video', src: "https://cdn.shopify.com/videos/c/o/v/a71367fe19884b209e96bc77f89cbcc4.mp4" },
+    { type: 'image', src: "https://cdn.shopify.com/s/files/1/0653/9830/9053/files/Slider_banner_2_mobile_35d40a8c-6630-457f-a181-e837812458cf.jpg?v=1738904905" },  
+    { type: 'video', src: "https://cdn.shopify.com/videos/c/o/v/993224c1f1b24bceb7b3b38efe81bd57.mp4" },
   ];
 
   const desktopContent = [
+    { type: 'video', src: "https://cdn.shopify.com/videos/c/o/v/4b2eea124c14451f9d85605c411abe09.mp4" },
     { type: 'image', src: "https://cdn.shopify.com/s/files/1/0653/9830/9053/files/Slider_banner_2_b15b76a2-c104-406d-be51-b70d53c7422d.jpg?v=1738758495" },
-    { type: 'video', src: "https://cdn.shopify.com/videos/c/o/v/cda1ad9527b3403e852d0dbd1ce058ee.mp4" }, 
+    { type: 'video', src: "https://cdn.shopify.com/videos/c/o/v/9a8de563f1824ef5901e2e685d2fff3b.mp4" }, 
   ];
 
   // Choose content based on screen size
@@ -53,27 +55,27 @@ const Banner = () => {
   }, []);
 
   return (
-    <div style={{ marginTop: '-80px' }}>
+    <div className=' h-[100vh]'>
     <Slider {...sliderSettings}>
       {content.map((item, index) => (
         <div key={index} className="w-full">
           {item.type === 'image' ? (
             <img
               loading="lazy"
-              className="w-full object-contain"
+              className="w-full object-contain h-[100vh]"
               src={item.src}
               alt={`Slide ${index + 1}`}
-              style={{ height: '1019px' }}
+             
             />
           ) : (
             <video
               ref={(el) => (videoRefs.current[index] = el)}
-              className="w-full object-contain"
+              className="w-full object-cover h-[100vh]"
               autoPlay
               loop
               muted
               playsInline
-              style={{ width: '1410px', height: '1090px' }}
+             
               onLoadedData={() => {
                 videoRefs.current[index]?.play().catch((err) => console.log("Autoplay prevented:", err));
               }}
@@ -85,8 +87,10 @@ const Banner = () => {
         </div>
       ))}
     </Slider>
-    <Banner2/>
-    </div>);
+    
+    </div>
+    
+  );
 };
 
 export default Banner;
