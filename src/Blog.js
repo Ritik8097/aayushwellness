0c5a979f4a08dd0dd4124f91392b76e313cd4c58
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-12 17:52:48 +0530

    Update Blog.js

--- src/Blog.js
+++ src/Blog.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Slider from "react-slick";
-import { Link } from "react-router-dom";  
+import Slider from "react-slick"; 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Blog.css";
